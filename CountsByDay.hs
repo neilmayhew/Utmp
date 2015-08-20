6c49ce0cf9b018b6e30e3ddb6fcf2c195a63d707
Neil Mayhew <neiljhmayhew@gmail.com>
2015-08-20 16:00:35 -0600

    Implement CountsByDay

---
+++ CountsByDay.hs
@@ -0,0 +1,21 @@
+import Utmp
+
+import Control.Monad
+import Data.Function
+import Data.List
+import Data.Time.Clock
+import System.Environment
+import Text.Printf
+
+-- Reads input from utmp files
+
+main = do
+    logins <- concat <$> (mapM decodeFile =<< getArgs)
+
+    let days = groupBy ((==) `on` utctDay . utTime) $ logins
+
+    forM_ days $ \ls -> do
+        let n = length ls
+        putStrLn $ printf "%s %d" (show . utctDay . utTime $ head ls) n
+
+    putStrLn $ printf "%10s %d" "" $ length logins
